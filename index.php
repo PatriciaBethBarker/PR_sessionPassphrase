bf28b6e7df52026fbc4bb4a766c930b2df63dc3b
Patricia <patriciabethbarker@gmail.com>
2015-06-15 16:53:39 -0700

    update index.php
    Add PHP Documentor

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
+/**
+ * index.php for Session Passphrase
+ * 
+ * 
+ * 
+ *
+ * @package CodeIgintor:  SMALL_PIECE_OF_PROGRAM
+ * @subpackage Session Passphrase
+ * @author Patricia Barker <patriciabethbarker@gmail.com>
+ * @version 2.0 2015/06/15 
+ * @link http://www.tcbcommercialproperties.com/sandbox/sp/
+ * @license http://www.apache.org/licenses/LICENSE-2.0
+ * @see login.php
+ * @see logout.php
+ * @see passwords.php
+ * @todo none
+ */
 <?php
 	SESSION_start();
 //this goes in front of every session 
