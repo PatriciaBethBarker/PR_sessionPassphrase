d71e8faa31bbd7b8c8695f00bdce95bb7360a6e1
Patricia <patriciabethbarker@gmail.com>
2015-06-15 16:56:31 -0700

    Update passwords.php
    Add PHP Documentor

--- passwords.php
+++ passwords.php
@@ -1,3 +1,20 @@
+/**
+ * passwords.php for Session Passphrase
+ * 
+ * 
+ * 
+ *
+ * @package CodeIgintor:  SMALL_PIECE_OF_PROGRAM
+ * @subpackage Session Passphrase
+ * @author Patricia Barker <patriciabethbarker@gmail.com>
+ * @version 2.0 2015/06/15 
+ * @link http://www.tcbcommercialproperties.com/sandbox/sp/
+ * @license http://www.apache.org/licenses/LICENSE-2.0
+ * @see login.php
+ * @see logout.php
+ * @see index.php
+ * @todo none
+ */
 <?php
   $password = "abc123";
 ?>
